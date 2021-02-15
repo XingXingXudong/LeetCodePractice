8e010911351f4984c6a6222984bcde5b886c614b
Xudong <xingxingxudong@gmail.com>
2021-02-15 19:50:45 +0800

    add data strcuter

---
+++ 10_data_strcuter/find_all_numbers_disappeared_in_an_array_448.cpp
@@ -0,0 +1,68 @@
+#include <gtest/gtest.h>
+#include <vector>
+#include <set>
+
+void print_vec(const std::vector<int>& vec) {
+    for (auto x: vec) {
+        std::cout << x << " ";
+    }
+    std::cout << std::endl;
+}
+
+bool is_equal(const std::vector<int>& a, const std::vector<int>& b) {
+    // std::cout << "a ";
+    // print_vec(a);
+    if (a.size() != b.size()) {
+        return false;
+    }
+    auto set_a = std::set<int>(a.begin(), a.end());
+    auto set_b = std::set<int>(b.begin(), b.end());
+    return set_a == set_b;
+}
+
+std::vector<int> findDisappearedNumbers(std::vector<int>& nums) {
+    auto record = std::vector<int>(nums.size() + 1, 0);
+    for (auto x: nums) {
+        ++record[x];
+    }
+    auto rtn = std::vector<int>{};
+    for (int i = 1; i < (int)record.size(); ++i) {
+        if (record[i] == 0) {
+            rtn.push_back(i);
+        }
+    }
+    return rtn;
+}
+
+std::vector<int> findDisappearedNumbers1(std::vector<int>& nums) {
+    auto rtn = std::vector<int>{};
+    for (auto n: nums) {
+        int pos = (n > 0 ? n : -n) - 1;
+        if (nums[pos] > 0) {
+            nums[pos] = -nums[pos];
+        }
+    }
+    for (int i = 0; i < (int)nums.size(); ++i) {
+        if (nums[i] > 0) {
+            rtn.push_back(i + 1);
+        }
+    }
+    return rtn;
+}
+
+TEST(findDisappearedNumbers, a) {
+    auto inputs = std::vector<int>{4,3,2,7,8,2,3,1};
+    auto outputs = std::vector<int>{5,6};
+    EXPECT_TRUE(is_equal(findDisappearedNumbers(inputs), outputs));
+}
+
+TEST(findDisappearedNumbers1, a) {
+    auto inputs = std::vector<int>{4,3,2,7,8,2,3,1};
+    auto outputs = std::vector<int>{5,6};
+    EXPECT_TRUE(is_equal(findDisappearedNumbers1(inputs), outputs));
+}
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ 10_data_strcuter/rotate_image_48.cpp
@@ -0,0 +1,62 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+void print_matrix(const std::vector<std::vector<int>>& m) {
+    std::cout << "------------------------------" << std::endl;
+    for (size_t i = 0; i < m.size(); ++i) {
+        for (size_t j = 0; j < m[i].size(); ++j) {
+            std::cout << m[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "------------------------------" << std::endl;
+}
+
+void rotateImage(std::vector<std::vector<int>>& matrix) {
+    int temp = 0, n = matrix.size() - 1;
+    for (int i = 0; i <= n / 2; ++i) {
+        for (int j = i; j < n - i; ++j) {
+            temp = matrix[i][j];
+            matrix[i][j] = matrix[n-j][i];
+            matrix[n-j][i] = matrix[n-i][n-j];
+            matrix[n-i][n-j] = matrix[j][n-i];
+            matrix[j][n-i] = temp;
+        }
+    }
+    return;
+}
+
+TEST(rotateImage, a) {
+    auto inputs = std::vector<std::vector<int>>{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+    auto output = std::vector<std::vector<int>>{{7, 4, 1}, {8, 5, 2}, {9, 6, 3}};
+    rotateImage(inputs);
+    EXPECT_TRUE(inputs == output);
+}
+
+TEST(rotateImage, b) {
+    auto inputs = std::vector<std::vector<int>>{{5,1,9,11},{2,4,8,10},{13,3,6,7},{15,14,12,16}};
+    auto output = std::vector<std::vector<int>>{{15,13,2,5},{14,3,4,1},{12,6,8,9},{16,7,10,11}};
+    rotateImage(inputs);
+    EXPECT_TRUE(inputs == output);
+}
+
+TEST(rotateImage, c) {
+    auto inputs = std::vector<std::vector<int>>{{1}};
+    auto output = std::vector<std::vector<int>>{{1}};
+    rotateImage(inputs);
+    EXPECT_TRUE(inputs == output);
+}
+
+TEST(rotateImage, d) {
+    auto inputs = std::vector<std::vector<int>>{{1,2},{3,4}};
+    auto output = std::vector<std::vector<int>>{{3,1},{4,2}};
+    rotateImage(inputs);
+    EXPECT_TRUE(inputs == output);
+}
+
+
+int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
