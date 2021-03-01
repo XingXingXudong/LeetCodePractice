b9a219300b152d2e26bf4a63ffb5b5e7526099f7
Liuxudong <17801036039@163.com>
2021-03-01 23:42:51 +0800

    update

--- 03_binary_search/split_array_largest_sum.cpp
+++ 03_binary_search/split_array_largest_sum.cpp
@@ -2,6 +2,39 @@
 
 #include <gtest/gtest.h>
 #include <vector>
+#include <numeric>
+
+bool check(std::vector<int>nums, int m, int mid) {
+    int sum = 0, cnt = 1;
+    for (int x: nums) {
+        if (x > mid) {
+            return false;
+        }
+        if (sum + x <= mid) {
+            sum += x;
+        } else {
+            sum = x;
+            ++cnt;  // 增加分计数
+        }
+    }
+    return cnt <= m;
+}
+
+int split(std::vector<int>nums, int m) {
+    int l = 0; 
+    int r = std::accumulate(nums.begin(), nums.end(), 0);
+    int ans = 0;
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (check(nums, m, mid)) {
+            ans = mid;
+            r = mid - 1;
+        } else {
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
 
 TEST(split, a) {
     auto nums = std::vector<int>{7,2,5,10,8};
